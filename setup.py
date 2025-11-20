d946c7e2b924fcf034a5b1853c1cfcac5aeebbf3
hiro <hiroto117@Hirotovivobook.localdomain>
2025-11-20 12:31:45 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'mkpkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Hiroto Miura',
+    maintainer_email='hiroto117@todo.com',
+    description='ロボットシステム学授業',
+    license='BSD-3-Clause',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'talker = mkpkg.talker:main',
+            'listener = mkpkg.listener:main',
+        ],
+    },
+)
